2d9e5a890ea323e43dd5e95cbca923abfd8ea239
mbsii3 <mshortjr3@gmail.com>
2023-04-06 14:48:32 -0500

    connect add task form and change functions

--- src/pages/NewTaskPage/NewTaskPage.jsx
+++ src/pages/NewTaskPage/NewTaskPage.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 export default function NewTaskPage() {
+    const [task, setTask] = useState({
+        name: '',
+        description: ''
+    });
+
+    function handleChange(e) {
+        setTask({ ...task, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert('clicked')
+    }
+
+
     return (
         <>
             <h1>Add Task</h1>
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
                 <label htmlFor="name">Task Name:
-                    <input type="text" name="name" id="name" />
+                    <input type="text" name="name" id="name" value={ task.name } onChange={handleChange}/>
                 </label>
                 <br />
-                <label htmlFor="description">Task Description
+                <label htmlFor="description">Description:
                     <input type="text" name="description" id="description" />
                 </label>
                 <br />
